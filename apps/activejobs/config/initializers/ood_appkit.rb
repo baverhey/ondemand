b2a4b05deabf95bf0b7e63d4f2c1cbf5c3a91999
Trey Dockendorf <tdockendorf@osc.edu>
2020-02-24 09:53:09 -0500

    Allow theme to be set for grafana graphs

--- apps/activejobs/config/initializers/ood_appkit.rb
+++ apps/activejobs/config/initializers/ood_appkit.rb
@@ -1,6 +1,7 @@
 # config/initializers/ood_appkit.rb
 require "ood_core"
 
+#ENV['OOD_CLUSTERS'] = '/users/sysp/tdockendorf/git/ondemand/apps/activejobs/clusters.d'
 OODClusters = OodCore::Clusters.new(
   OodAppkit.clusters.select(&:job_allow?)
 )
